c62ee9284dcdd182e11745d476ea7e102492bf15
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-11 17:47:16 +0545

    d

--- practise/src/app/layout.js
+++ practise/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}</body>
+      <Navbar/>
+        {children}
+        <Footer/>
+        </body>
     </html>
   );
 }
