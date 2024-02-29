02eaeecaa83353b780cc6347a1cbfb03b2ca8c22
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-29 20:31:30 +0545

    api test in postman

--- project/src/app/api/users/route.js
+++ project/src/app/api/users/route.js
@@ -5,3 +5,12 @@ export function GET(request){
     const data = user;
     return NextResponse.json(data)
 }
+
+export async function POST(request, response){
+    let payload = await request.json()
+    console.log(payload)
+    if(!payload.name || !payload.age || !payload.email){
+        return NextResponse.json({result:"required field",succes:false})
+    }
+    return NextResponse.json({result:"User created succesfullyy",success:true})
+}
