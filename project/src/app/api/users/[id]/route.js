40fedd4021bd7437fdcc6c3958d5cf7098bfe39d
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-03 14:25:30 +0545

    checking body data in console

--- project/src/app/api/users/[id]/route.js
+++ project/src/app/api/users/[id]/route.js
@@ -12,3 +12,12 @@ export function GET(request,content){
     return NextResponse.json(
         userData.length==0?{result:"no data found",success:false}:{result:userData,success:true},{status:200})
 }
+
+export async function PUT(request, content){
+    let payload =await request.json()
+
+    payload.id = content.params.id;
+    console.log(payload);
+
+    return NextResponse.json({result:true});
+}
