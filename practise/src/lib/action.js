cf46bd2d7a41a0060b63e41aef951905f38536cb
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-10 08:54:57 +0545

    getting data from formdata through props

--- practise/src/lib/action.js
+++ practise/src/lib/action.js
@@ -1,4 +1,10 @@
- export const sayHello = async() =>{
+ export const addPost = async(formData) =>{
 "use server";
-console.log("hello");
+
+// const title=formData.get("title")
+// const desc=formData.get("desc")
+// const slug=formData.get("slug")
+
+const {title, desc,slug} = Object.fromEntries(formData);
+console.log(title,desc,slug);
  }
