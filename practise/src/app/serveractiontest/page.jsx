9a284b93494c6321137faa2e9573de4018050f44
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-10 08:55:49 +0545

    creating form to send data directly through component function

--- practise/src/app/serveractiontest/page.jsx
+++ practise/src/app/serveractiontest/page.jsx
@@ -1,10 +1,18 @@
-import {sayHello} from "@/lib/action"
+import {addPost} from "@/lib/action"
 
  const ServerActionTestPage = () => {
+    // const actionInComponent = async() =>{
+    //     "use server"
+    //     console.log("it works")
+    // }
     return(
         <div>
-            <form action={sayHello}>
-                <button>Test hello</button>
+            <form action={addPost}>
+                <input type="text" name="title" placeholder="title"/>
+                <input type="text" name="desc" placeholder="desc"/>
+                <input type="text" name="slug" placeholder="slug"/>
+                <input type="text" name="userid" placeholder="userId"/>
+                <button>Create</button>
             </form>
         </div>
     )
